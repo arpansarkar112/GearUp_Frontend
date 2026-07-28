13fdbffea70b1603457d42c58c88d6de770ae131
Arpan Sarkar <arpansarkar112@gmail.com>
2026-07-28 21:35:42 +0300

    feat: setup auth pages, role-based dashboard layouts, and project folder placeholders primarilly to check if the routes are working fine

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Auth Login Page </h1>
+      <p>URL: /auth/login</p>
+    </div>
+  );
+}
---
+++ app/auth/register/page.tsx
@@ -0,0 +1,8 @@
+export default function RegisterPage() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Auth Register Page</h1>
+      <p>URL: /auth/register</p>
+    </div>
+  );
+}
